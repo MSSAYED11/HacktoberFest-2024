1cf8199b70fdbf3f6572fe4b29cf99df53586346
MSSAYED11 <mssayed1108@gmail.com>
2024-10-31 19:41:22 +0530

    Create Arraysort.java

---
+++ JAVA/Arraysort.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class ArraySort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 3};
+        Arrays.sort(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
